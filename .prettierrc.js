8dba8ad4cedd73a900f89fa8934cf1474bd2efcb
erlei22 <574414066@qq.com>
2020-08-06 17:45:56 +0800

    style: add prettier   semi: false

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: erlei22
+ * @Date: 2020-08-06 16:07:44
+ * @LastEditors: erlei22
+ * @LastEditTime: 2020-08-06 17:45:09
+ * @FilePath: /.prettierrc.js
+ */
 const fabric = require('@umijs/fabric');
 
 module.exports = {
   ...fabric.prettier,
+   semi: false,
 };
